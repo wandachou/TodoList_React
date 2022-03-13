2ee45b0cc98f13f38d4ea9da328891df1ee9b92d
Wanda Chou <80566859+wandachou@users.noreply.github.com>
2022-03-13 17:23:26 +0800

    fix: build 路徑錯誤問題

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import TodoApp from "./TodoApp";
+import "./styles.css";
 
 const rootElement = document.getElementById("root")
 
